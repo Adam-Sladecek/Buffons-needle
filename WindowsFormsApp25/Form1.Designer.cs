369066a7b76be5c10c3dc1e7719038e2c185d11b
xhyphenx <adamsladecek0@gmail.com>
2021-02-07 14:56:07 +0100

    pridanie projektu

---
+++ WindowsFormsApp25/Form1.Designer.cs
@@ -0,0 +1,194 @@
+﻿namespace WindowsFormsApp25
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.backgroundWorker2 = new System.ComponentModel.BackgroundWorker();
+            this.trackBar1 = new System.Windows.Forms.TrackBar();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.button1.Location = new System.Drawing.Point(1068, 504);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(154, 45);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Štart";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label1.Location = new System.Drawing.Point(981, 22);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(358, 23);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Number of intersections : ";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label2.Location = new System.Drawing.Point(981, 66);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(358, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Number of throws : ";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label3.Location = new System.Drawing.Point(981, 107);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(356, 23);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Aproxiamation : ";
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // backgroundWorker2
+            // 
+            this.backgroundWorker2.WorkerSupportsCancellation = true;
+            this.backgroundWorker2.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker2_DoWork);
+            // 
+            // trackBar1
+            // 
+            this.trackBar1.Location = new System.Drawing.Point(986, 366);
+            this.trackBar1.Maximum = 1000;
+            this.trackBar1.Name = "trackBar1";
+            this.trackBar1.Size = new System.Drawing.Size(310, 45);
+            this.trackBar1.TabIndex = 5;
+            this.trackBar1.Tag = "";
+            this.trackBar1.TickFrequency = 100;
+            this.trackBar1.Value = 500;
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label4.Location = new System.Drawing.Point(1103, 340);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(78, 23);
+            this.label4.TabIndex = 6;
+            this.label4.Text = "Speed";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label5.Location = new System.Drawing.Point(959, 398);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(45, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "fast";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label6.Location = new System.Drawing.Point(1280, 398);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(57, 23);
+            this.label6.TabIndex = 8;
+            this.label6.Text = "slow";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label7.Location = new System.Drawing.Point(981, 152);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(356, 23);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Real value :  3.14159265359...";
+            // 
+            // label8
+            // 
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.label8.Location = new System.Drawing.Point(981, 200);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(356, 23);
+            this.label8.TabIndex = 10;
+            this.label8.Text = "Deviation :";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1372, 961);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.trackBar1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.MinimizeBox = false;
+            this.Name = "Form1";
+            this.Tag = "";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.Paint += new System.Windows.Forms.PaintEventHandler(this.Form1_Paint);
+            ((System.ComponentModel.ISupportInitialize)(this.trackBar1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer timer1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker2;
+        private System.Windows.Forms.TrackBar trackBar1;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+    }
+}
+
---
+++ WindowsFormsApp25/Form1.cs
@@ -0,0 +1,184 @@
+﻿using System;
+using System.ComponentModel;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp25
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        Random rnd = new Random();
+        
+       
+        Pen pen = new Pen(Color.Red, 5f);
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            sum_of_intersections = 0;
+            number_of_throws = 0;
+            space_between_lines = 100;
+            number_of_lines = 10;
+            needle_length = space_between_lines / 2;
+            switch1 = 0;
+            switch2 = 0;
+
+            timer1.Enabled = true;
+            timer1.Interval = 1;
+           
+            backgroundWorker2.WorkerSupportsCancellation = true;
+            
+          
+           
+            
+        }
+        int space_between_lines, number_of_lines, needle_length, sum_of_intersections, time_interval, switch1, switch2, number_of_throws;
+        double ratio;
+        private void Form1_Paint(object sender, PaintEventArgs e)
+        {
+           int i;
+           
+            
+            
+                Graphics g = e.Graphics;
+                for (i = 0; i <= number_of_lines - 1; i++)
+                {
+                    g.DrawLine(Pens.Black, (i * space_between_lines), 0, (i * space_between_lines), ((number_of_lines - 1) * space_between_lines));
+
+                }
+            
+            
+         
+        }
+        
+        
+       
+        Point b1, b2;
+       
+        private void Animation()
+        {
+            while (true)
+            {
+                if (backgroundWorker2.CancellationPending)
+                {
+                    break;
+                }
+                else
+                {
+                    Thread.Sleep(time_interval);
+
+                    int xsur, ysur, i;
+                  
+                    xsur = rnd.Next(space_between_lines * (number_of_lines - 1));
+                    ysur = rnd.Next(space_between_lines * (number_of_lines - 1));
+                    
+                    (Point, Point) g;
+                    g = line_segment(xsur, ysur);
+                    b1 = g.Item1;
+                    b2 = g.Item2;
+                    pen.Color = Color.Red;
+                    
+                    if (is_intersecting(p1, p2))
+                    {
+                        sum_of_intersections += 1;
+                        pen.Color = Color.Blue;
+                    }
+
+                    number_of_throws += 1;
+
+                    
+                    ratio = (2.0 * needle_length) / (space_between_lines * (Convert.ToDouble(sum_of_intersections) / Convert.ToDouble(number_of_throws)));
+                    Graphics t = CreateGraphics();
+                    t.DrawLine(pen, b1.X, b1.Y, b2.X, b2.Y);
+              
+                    
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            switch1 = 1 - switch2;
+            switch2 = 1;
+            if (switch1 == 1)
+            {
+                button1.Text = "Stop";
+                backgroundWorker2.RunWorkerAsync();
+            }
+            else
+            {
+                button1.Text = "Štart";
+                switch2 = 0;
+                backgroundWorker2.CancelAsync();
+            }
+           
+
+        }
+
+        Point middle = new Point();
+        Point p1 = new Point();
+        Point p2 = new Point();
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+
+
+            label1.Text = "Number of intersections : " + Convert.ToString(sum_of_intersections);
+            label2.Text = "Number of throws : " + Convert.ToString(number_of_throws);
+             label3.Text = "Aproxiamation : " + Convert.ToString(ratio);
+            label8.Text = "Deviation : " + Math.Abs(Math.PI - ratio).ToString();
+            time_interval = trackBar1.Value;
+           
+
+        }
+
+      
+
+        private void backgroundWorker2_DoWork(object sender, DoWorkEventArgs e)
+        {
+            Animation();
+            
+        }
+
+
+        double rand_angle;
+       
+       
+        
+        (Point,Point) line_segment (int xx,int yy)
+        {
+            
+           
+            middle.X = xx;
+            middle.Y = yy;
+            
+            rand_angle = rnd.NextDouble()*(Math.PI);
+            
+            p1.X = middle.X + Convert.ToInt32(needle_length / 2* Math.Cos(rand_angle));
+            p1.Y = middle.Y + Convert.ToInt32(needle_length / 2 * Math.Sin(rand_angle));
+           
+            p2.X = middle.X - Convert.ToInt32(needle_length / 2 * Math.Cos(rand_angle));
+            p2.Y = middle.Y - Convert.ToInt32(needle_length / 2 * Math.Sin(rand_angle));
+            return (p1,p2) ;  
+
+        }
+        bool is_intersecting (Point a,Point b)
+        {
+            int i;
+            
+            for (i=0;i<=(number_of_lines - 1)* space_between_lines; i+= space_between_lines)
+            {
+             
+                if ((a.X<=i && b.X>=i) || (b.X <= i && a.X >= i))
+                {
+                    return true;
+                }
+                
+                
+            }
+            return false;
+        }
+    }
+}
